bcf8278d4914ce0e6e57eaa137d413d5d3750261
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-11-17 17:38:49 +0100

    BugFix

--- dependency/cms-api/src/routes/apiRouter.ts
+++ dependency/cms-api/src/routes/apiRouter.ts
@@ -1,10 +1,11 @@
 import { Request, Response, Router } from 'express';
 import DB_utils from '../utils/DB_utils';
+import { ContestInfo } from '../utils/interfaces';
 
 const router = Router();
 
 router.get('/contests', async (req: Request, res: Response) => {
-  const contestsInfo = DB_utils.getContestsInfo();
+  const contestsInfo: ContestInfo[] = await DB_utils.getContestsInfo();
   res.set("Content-Type", "application/json");
   res.send(JSON.stringify(contestsInfo));
 });
