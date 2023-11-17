ca276a53481f283b986e3688516557cd3f7d1d2e
CawaAlreadyTaken <daniele.cabassi01@gmail.com>
2023-11-17 16:51:09 +0100

    Added correct host for cms-db

--- dependency/cms-api/src/utils/PostgresConnection.ts
+++ dependency/cms-api/src/utils/PostgresConnection.ts
@@ -1,5 +1,7 @@
 import { Pool, QueryResult } from 'pg';
 
+const HOST_IP = "cms-db";
+
 class PostgresConnection {
   private static instance: PostgresConnection;
   private pool: Pool;
@@ -8,7 +10,7 @@ class PostgresConnection {
   private constructor() {
     this.pool = new Pool({
       user: process.env.POSTGRES_USER,
-      host: 'localhost',
+      host: HOST_IP,
       database: process.env.POSTGRES_DB,
       password: process.env.POSTGRES_PASSWORD,
       port: 5432,
